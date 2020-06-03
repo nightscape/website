efc5ff12d18876dc0fe6898a5298441db77fd7c9
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-06-03 02:46:41 +0000

    [index] add icon to section "Highly Secure".

--- src/components/index/HighlySecure.tsx
+++ src/components/index/HighlySecure.tsx
@@ -3,8 +3,16 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled from '@emotion/styled'
 import PatternLong from '../../resources/pattern-long.png'
+import HighlySecureIcon from '../../resources/highly-secure.svg'
 
 const Styled = styled.section`
+
+    img {
+        display: block;
+        height: 8rem;
+        margin: 0 auto 3rem;
+    }
+
     h2 + p {
         margin: 0;
     }
@@ -22,6 +30,7 @@ const Styled = styled.section`
 const HighlySecure = () => (
     <Styled>
         <div className="row">
+            <img src={HighlySecureIcon} alt="Sheild"/>
             <h2><strong>Highly Secure</strong></h2>
             <p>The safety of your data is crucial for us. Gitpod uses SSL technology to protect your data using both server authentication and strong encryption standards in transit.</p>
             <p>More about <Link to="/">Security</Link>.</p>
