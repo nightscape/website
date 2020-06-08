d1495627afdf28b18ccf53a1cebc139fc90afcde
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-06-08 03:07:23 +0000

    tagline circle add the galaxy background.

--- src/components/index/Tagline.tsx
+++ src/components/index/Tagline.tsx
@@ -17,9 +17,10 @@ const Styled = styled.section`
     }
 
     .bar {
-        &-container {
-            flex: 0 0 38%;
 
+        &-wrapper {
+            flex: 0 0 38%;
+            
             @media(max-width: 960px) {
                 flex: 0 0 32%;
                 transform: scale(.8);
@@ -30,6 +31,34 @@ const Styled = styled.section`
             }
         }
 
+        &-container {
+            position: relative;
+            display: inline-block;
+            transform: rotate(-45deg);
+
+            &::before {
+                content: '';
+                position: absolute;
+                display: block;
+                top: 0;
+                left: 0;
+                right: 0;
+                height: 100%;
+                background: url(galaxy-big.jpg);
+                background-size: cover;
+                z-index: -1;
+                border-radius: 50%;
+                transform: translateY(-100%);
+                opacity: 0;
+                transition: all .2s;
+            }
+
+            &:hover::before {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+
         display: flex;
         justify-content: center;
         align-items: center;
@@ -37,7 +66,24 @@ const Styled = styled.section`
         width: 260px;
         font-size: 5rem;
         border: 35px solid ${colors.offWhite2};
-        border-radius: 50%
+        border-radius: 50%;
+
+        &:hover {
+            border-right-color: transparent;
+            transition: all .2s;
+        }
+    }
+
+    .content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        background: ${colors.white};
+        border-radius: 50%;
+        transform: scale(1.01);
+        transform: rotate(45deg);
     }
 
     .text {
@@ -53,10 +99,14 @@ const Styled = styled.section`
 const Tagline = () => (
     <div className="row">
         <Styled>
-            <div className="bar-container">
-                <div className="bar">
-                    <strong>24</strong>%
-            </div>
+            <div className="bar-wrapper">
+                <div className="bar-container">
+                    <div className="bar">
+                        <div className="content">
+                            <strong>24</strong>%
+                    </div>
+                    </div>
+                </div>
             </div>
             <div className="text">
                 <h2><strong>Increase your productivity up to 24%</strong> with prebuilts & automated dev environments.</h2>
